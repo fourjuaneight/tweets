f437f6de6d5bdf84d3b62727a8a756c12aba30b1
Juan Villela <juan@villela.co>
2022-04-06 17:25:12 -0400

    feat: Add tweets query function.

---
+++ functions/api/tweets.ts
@@ -0,0 +1,99 @@
+interface TweetValues {
+  id?: string;
+  tweet: string;
+  date: string;
+  url: string;
+}
+
+interface TweetRecord {
+  id: string;
+  fields: TweetValues;
+  createdTime: string;
+}
+
+interface AirtableTweetsResp {
+  records: TweetRecord[];
+  offset: string;
+}
+
+interface ContextValue {
+  [key: string]: string;
+}
+
+interface RequestParams {
+  env: ContextValue;
+}
+
+let data: TweetValues[] = [];
+
+const getTweetsWithOffset = (
+  env: ContextValue,
+  offset?: string
+): Promise<AirtableTweetsResp> => {
+  const options: RequestInit = {
+    headers: {
+      Authorization: `Bearer ${env.AIRTABLE_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  };
+  const endpoint = `https://api.airtable.com/v0/${env.AIRTABLE_MEDIA_ID}/Tweets`;
+  const url = offset ? `${endpoint}?offset=${offset}` : endpoint;
+
+  try {
+    return fetch(url, options)
+      .then((response: Response) => response.json())
+      .then((airtableRes: AirtableTweetsResp) => {
+        data = [
+          ...data,
+          ...airtableRes.records.map(({ fields }) => {
+            const id = fields.url.replace(
+              /(https\:\/\/twitter\.com\/fourjuaneight\/status\/)(.*)/g,
+              '$2'
+            );
+
+            return {
+              id,
+              ...fields,
+            };
+          }),
+        ];
+
+        if (airtableRes.offset) {
+          return getTweetsWithOffset(env, airtableRes.offset);
+        }
+
+        return airtableRes;
+      });
+  } catch (error) {
+    throw `(getTweetsWithOffset) - ${error}`;
+  }
+};
+
+export const onRequestGet = async ({ env }: RequestParams) => {
+  try {
+    await getTweetsWithOffset(env);
+
+    if (data.length) {
+      return new Response(JSON.stringify(data), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        ok: true,
+        status: 200,
+      });
+    } else {
+      return new Response('Missing data', {
+        ok: false,
+        status: 500,
+      });
+    }
+  } catch (error) {
+    return new Response(error, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      ok: false,
+      status: 500,
+    });
+  }
+};
