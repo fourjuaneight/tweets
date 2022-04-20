145fa3ddf8027505b3069c2f2103ce9730ccc53b
Juan Villela <juan@villela.co>
2022-04-20 14:47:47 -0400

    feat: Add relative date filter.

--- utils/filters.js
+++ utils/filters.js
@@ -1,4 +1,11 @@
-const { format, formatISO, getYear } = require('date-fns');
+const {
+  format,
+  formatISO,
+  getYear,
+  formatRelative,
+  parseISO,
+  differenceInDays,
+} = require('date-fns');
 
 const meta = require('../src/data/meta');
 
@@ -7,6 +14,17 @@ module.exports = {
   cleanURL: url => url.replace(/\.html$/g, ''),
   dateToFormat: (date, fmt) => format(date, fmt),
   dateToISO: date => formatISO(date).slice(0, -5),
+  dateToRelative: date => {
+    const distance = differenceInDays(parseISO(date), new Date());
+
+    if (distance < -6) {
+      return format(parseISO(date), 'PPPp');
+    } else {
+      return formatRelative(parseISO(date), new Date(), {
+        weekStartsOn: 1,
+      });
+    }
+  },
   dateYear: dateStr => {
     const date = new Date(dateStr);
     return getYear(date);
