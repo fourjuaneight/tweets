ab88902d9773ee21bf838bb11a310a06a2124a7a
Juan Villela <juan@villela.co>
2022-04-20 15:28:17 -0400

    feat: Create tweet years list data file.

--- helpers/tweets-data.ts
+++ helpers/tweets-data.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 import chalk from 'chalk';
 import dotenv from 'dotenv';
 import fetch from 'isomorphic-fetch';
+import { getYear, parseISO } from 'date-fns';
 
 interface Tweet {
   id: string;
@@ -21,9 +22,13 @@ const { writeFile } = promises;
   try {
     const response = await fetch('https://tweets.juanvillela.dev/api/tweets/');
     const data: Tweet[] = await response.json();
+    const years: number[] = data
+      .map(({ date }) => getYear(parseISO(date)))
+      .filter((value, index, self) => self.indexOf(value) === index);
 
     await writeFile(`${dist}/tweets.json`, JSON.stringify(data, null, 2));
+    await writeFile(`${dist}/tweet-years.json`, JSON.stringify(years, null, 2));
   } catch (error) {
-    throw new Error(`${chalk.red('[ERROR]')} ${error}`);
+    throw new Error(`${error}`);
   }
 })();
