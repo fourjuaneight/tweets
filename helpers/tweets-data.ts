33c1a440d04e98738280cc53b9b4dba35774aab0
Juan Villela <juan@villela.co>
2022-04-20 15:31:20 -0400

    fix: Use camelCase data file names.

--- helpers/tweets-data.ts
+++ helpers/tweets-data.ts
@@ -27,7 +27,7 @@ const { writeFile } = promises;
       .filter((value, index, self) => self.indexOf(value) === index);
 
     await writeFile(`${dist}/tweets.json`, JSON.stringify(data, null, 2));
-    await writeFile(`${dist}/tweet-years.json`, JSON.stringify(years, null, 2));
+    await writeFile(`${dist}/tweetYears.json`, JSON.stringify(years, null, 2));
   } catch (error) {
     throw new Error(`${error}`);
   }
